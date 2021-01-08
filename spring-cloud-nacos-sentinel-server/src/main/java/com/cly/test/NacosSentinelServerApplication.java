e999822687f0b74b78449d2251e8bd9a6e7de44f
cly <2313681959@qq.com>
2021-01-08 14:37:20 +0800

    sentinel

--- spring-cloud-nacos-sentinel-server/src/main/java/com/cly/test/NacosSentinelServerApplication.java
+++ spring-cloud-nacos-sentinel-server/src/main/java/com/cly/test/NacosSentinelServerApplication.java
@@ -1,14 +1,17 @@
 package com.cly.test;
 
 
+import com.alibaba.cloud.sentinel.datasource.RuleType;
+import com.alibaba.csp.sentinel.EntryType;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @Slf4j
 @SpringBootApplication
@@ -19,7 +22,7 @@ public class NacosSentinelServerApplication {
         SpringApplication.run(NacosSentinelServerApplication.class, args);
     }
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    //@SentinelResource(value = "test.hello",fallback = "helloError")
+    @SentinelResource(value = "testHello",fallback = "helloError",blockHandler = "blockMethod")
     public String hello() {
        return "nacos balance";
     }
@@ -27,4 +30,19 @@ public class NacosSentinelServerApplication {
     public String helloError(String name, Throwable e){
         return "error,"+name;
     }
+    @RequestMapping(value ="getById/{id}",method = RequestMethod.GET)
+    //@SentinelResource(value="getRecord",blockHandler = "blockMethod")
+    public String getRecord(@PathVariable String id){
+        return id;
+    }
+
+    @RequestMapping(value ="/getById",method = RequestMethod.GET)
+    @SentinelResource(value="getRecord",fallback = "helloError",blockHandler = "blockMethod")
+    public String getRecord1(@RequestParam(value = "id",required = false)String id){
+        return id+"==================";
+    }
+    public String blockMethod(String id, BlockException e){
+        return "热点"+id+"error";
+    }
+
 }
